3e4d5f601bcf9f6d7e013d056a1f6880aa7ef23b
Ayush Mandowara <ayushxx7@gmail.com>
2020-07-18 18:57:05 +0530

    intermediate requirements complete (3/3) - article objects should be sortable by publication date added, added comments and removed junk code

--- qualifier.py
+++ qualifier.py
@@ -29,9 +29,11 @@ class ArticleField:
 
 class Article:
     """The `Article` class you need to write for the qualifier."""
+    
 
-    article_id = itertools.count()
+    article_id = itertools.count() #TC201: each article object should have unique id.
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
+        """TC101: set values from init"""
         self.id = next(Article.article_id)
         self.title = title
         self.author = author
@@ -41,42 +43,22 @@ class Article:
 
         return None
 
-    def update_last_editied(self, p_value):
-        print("update_last_editied:",p_value)
-        # if p_value != self.content: ## confusing.
-        self.last_edited = datetime.datetime.now()
-
-    
-    @property
-    def content(self):
-        # print(self._content)
-        return self._content
-
-    @content.setter
-    def content(self, value):
-        # print("value:",value)
-        self._content = value # the content isn't actually updating.
-        # print("self.content:",self.content)
-        self.update_last_editied(value) # just updating the last edited though.
-
-
-
     def __repr__(self):
-
+        """TC102: class should have repr function"""
         representation = f"<Article title=\"{self.title}\" author='{self.author}' publication_date='{self.publication_date.isoformat()}'>"
 
         return representation
 
     def __len__(self):
-
+        """TC103: class should have len function that checks length of content"""
         return len(self.content)
-    
-    def short_introduction(self, n_characters):
 
+    def short_introduction(self, n_characters):
+        """TC104: short intro that returns upto n complete worlds"""
         return textwrap.wrap(self.content, n_characters)[0]
-    
+
     def most_common_words(self, n):
-       
+        """TC105: generate stats of most common n case insensitive worlds. special chars are word boundaries"""
         lower_case_content = self.content.lower()
         split_all_except_lower_alpha = re.split(r'[^a-z]',lower_case_content)
         remove_empty_from_list = list(filter(None, split_all_except_lower_alpha))
@@ -84,4 +66,17 @@ class Article:
 
         return most_common_n_words
 
+    ### TC202: changing article content should update the last edited time.
+    @property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, value):
+        self._content = value 
+        self.last_edited = datetime.datetime.now()
+
+    ### TC203: articles should be sortable by publication date.
+    def __lt__(self, other):
+        return self.publication_date < other.publication_date
 
